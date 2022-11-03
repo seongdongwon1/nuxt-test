414103664b8d71fb16b0c00b515edeb7f2a91b82
sd <sdtv9999@gmail.com>
2022-11-03 17:09:04 +0900

    second commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,10 @@ export default {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
-
+    server: {
+        host: '0.0.0.0',
+        port: '8090'
+    },
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
     ],
