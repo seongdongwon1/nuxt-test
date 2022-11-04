d8c109bbecbb56e2d3c05eebd00cf901d546e16e
sd <sdtv9999@gmail.com>
2022-11-04 10:59:05 +0900

    pm2 nuxt config 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,8 +20,8 @@ export default {
         ]
     },
     server: {
-        host: '0.0.0.0',
-        port: '8090'
+        host: process.env.NUXT_HOST,
+        port: process.env.NUXT_PORT
     },
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
@@ -49,8 +49,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        retry: true,
-        baseURL: process.env.REST_API
+        baseURL: '/'
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
